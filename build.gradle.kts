4ff86549038b41ef477e8f85ee13abb977fd8429
Leonhardt Koepsell <hello@lnhrdt.com>
2024-10-28 11:36:51 -0500

    Configure test logging
    Authored-by: Leonhardt Koepsell <hello@lnhrdt.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
   `java-gradle-plugin`
   `jvm-test-suite`
@@ -24,6 +27,17 @@ testing {
       useJUnitJupiter(libs.versions.junit.jupiter)
       dependencies {
         implementation(libs.strikt.core)
+        implementation.bundle(libs.bundles.logging.implementation)
+        runtimeOnly.bundle(libs.bundles.logging.runtime)
+      }
+      targets.all {
+        testTask {
+          doFirst { fixTestKitLoggingInterference() }
+          testLogging {
+            events(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+            exceptionFormat = TestExceptionFormat.FULL
+          }
+        }
       }
     }
   }
@@ -42,4 +56,11 @@ tasks.named("check") {
     testing.suites.named("functionalTest"),
     testing.suites.named("platformTest"),
   )
+}
+
+// Workaround for https://github.com/gradle/gradle/issues/1893
+// Inspired by https://github.com/ratpack/ratpack/commit/0d9d2eb1d863a2a80b06c3e37f90cad1e7c4bdd1
+fun Test.fixTestKitLoggingInterference() {
+  val gradleTestKitFiles = (dependencies.gradleTestKit() as? FileCollectionDependency)?.files ?: emptySet()
+  classpath = files(classpath.files.sortedBy { it in gradleTestKitFiles })
 }
