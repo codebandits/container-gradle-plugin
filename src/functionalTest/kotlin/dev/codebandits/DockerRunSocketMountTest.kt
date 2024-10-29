7902550077d23978f2de3e477c3668f98cdf1d68
Leonhardt Koepsell <hello@lnhrdt.com>
2024-10-29 11:31:20 -0500

    Add volumes and refactor Docker host configurations
    Authored-by: Leonhardt Koepsell <hello@lnhrdt.com>

---
+++ src/functionalTest/kotlin/dev/codebandits/DockerRunSocketMountTest.kt
@@ -0,0 +1,50 @@
+package dev.codebandits
+
+import dev.codebandits.helpers.appendLine
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.contains
+import strikt.assertions.isEqualTo
+import strikt.assertions.isNotNull
+
+class DockerRunSocketMountTest : GradleProjectTest() {
+
+  @Test
+  fun `dockerRun volumes can be used to mount the docker socket`() {
+    gradleBuildFile.appendLine(
+      """
+      import dev.codebandits.ContainerRunTask
+      
+      plugins {
+        id("dev.codebandits.container")
+      }
+      
+      tasks {
+        register<ContainerRunTask>("dockerVersion") {
+          dockerRun {
+            image = "curlimages/curl:latest"
+            user = "root"
+            containerArgs = arrayOf("--unix-socket", "/var/run/docker.sock", "-sS", "http://./version")
+            volumes = arrayOf(
+              "/var/run/docker.sock:/var/run/docker.sock:ro",
+            )
+          }
+        }
+      }
+      """.trimIndent()
+    )
+
+    val result = GradleRunner.create()
+      .withPluginClasspath()
+      .withProjectDir(projectDirectory.toFile())
+      .withArguments("dockerVersion")
+      .build()
+
+    expectThat(result).and {
+      get { task(":dockerVersion") }.isNotNull().get { outcome }.isEqualTo(TaskOutcome.SUCCESS)
+      get { output }.contains("ApiVersion")
+    }
+  }
+}
--- src/main/kotlin/dev/codebandits/ContainerRunTask.kt
+++ src/main/kotlin/dev/codebandits/ContainerRunTask.kt
@@ -8,12 +8,16 @@ public abstract class ContainerRunTask : ContainerExecTask() {
   public open class DockerRunSpec(objects: ObjectFactory) {
     public val image: Property<String> = objects.property(String::class.java)
     public val entrypoint: Property<String> = objects.property(String::class.java)
+    public val volumes: Property<Array<String>> =
+      objects.property(Array<String>::class.java).convention(emptyArray())
     public val containerArgs: Property<Array<String>> =
       objects.property(Array<String>::class.java).convention(emptyArray())
     public val workdir: DirectoryProperty = objects.directoryProperty()
+    public val user: Property<String> = objects.property(String::class.java)
     public val autoRemove: Property<Boolean> = objects.property(Boolean::class.java).convention(true)
     public val alwaysPull: Property<Boolean> = objects.property(Boolean::class.java).convention(true)
-    public val daemonSocketPath: Property<String> = objects.property(String::class.java)
+    public val dockerHost: Property<String> =
+      objects.property(String::class.java).convention("unix:///var/run/docker.sock")
   }
 
   public fun dockerRun(configure: DockerRunSpec.() -> Unit) {
@@ -21,10 +25,16 @@ public abstract class ContainerRunTask : ContainerExecTask() {
     val image = spec.image.get()
     val containerArgs = spec.containerArgs.get()
     val options = mutableListOf<String>()
+    val user = spec.user.orNull
+    if (user != null) {
+      options.addAll(listOf("--user", user))
+    }
     if (spec.autoRemove.get()) {
       options.add("--rm")
     }
-    val daemonSocketPath = spec.daemonSocketPath.orNull
+    spec.volumes.get().forEach { volume ->
+      options.addAll(listOf("--volume", volume))
+    }
     val entrypoint = spec.entrypoint.orNull
     if (entrypoint != null) {
       options.addAll(listOf("--entrypoint", entrypoint))
@@ -39,9 +49,7 @@ public abstract class ContainerRunTask : ContainerExecTask() {
         execAction = {
           executable = Commands.dockerPath
           args(*dockerArgs)
-          if (daemonSocketPath != null) {
-            environment("DOCKER_HOST", "unix://$daemonSocketPath")
-          }
+          environment("DOCKER_HOST", spec.dockerHost.get())
         },
       )
     )
