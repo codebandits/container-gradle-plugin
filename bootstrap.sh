460eba0538155a964f759ef866034335f9563f3b
Leonhardt Koepsell <hello@lnhrdt.com>
2024-10-31 12:28:56 -0500

    Configure a repo bootstrap script
    Authored-by: Leonhardt Koepsell <hello@lnhrdt.com>

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e
+
+WRAPPER_PROPERTIES="./gradle/wrapper/gradle-wrapper.properties"
+WRAPPER_JAR="./gradle/wrapper/gradle-wrapper.jar"
+
+if [ ! -f "$WRAPPER_JAR" ]; then
+  echo "gradle-wrapper.jar is missing, downloading..."
+  DISTRIBUTION_URL=$(grep -o 'distributionUrl=.*' "$WRAPPER_PROPERTIES" | cut -d'=' -f2)
+  GRADLE_VERSION=$(echo "$DISTRIBUTION_URL" | sed -nE 's/.*gradle-(.+)-(bin|all)\.zip/\1/p')
+  JAR_URL="https://github.com/gradle/gradle/raw/refs/tags/v${GRADLE_VERSION}/gradle/wrapper/gradle-wrapper.jar"
+  curl -s -L -o "$WRAPPER_JAR" "$JAR_URL"
+  echo "gradle-wrapper.jar downloaded successfully."
+fi
