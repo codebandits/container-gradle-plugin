76d5882703cb1681372894e81424df3697c11219
Leonhardt Koepsell <hello@lnhrdt.com>
2025-04-22 14:38:13 +0700

    Update bootstrap script to handle non-semver
    Gradle names new minor versions without the 0 patch designation, but the
    GitHub tags for the release still require them.
    
    Authored-by: Leonhardt Koepsell <hello@lnhrdt.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,9 @@ if [ ! -f "$WRAPPER_JAR" ]; then
   echo "gradle-wrapper.jar is missing, downloading..."
   DISTRIBUTION_URL=$(grep -o 'distributionUrl=.*' "$WRAPPER_PROPERTIES" | cut -d'=' -f2)
   GRADLE_VERSION=$(echo "$DISTRIBUTION_URL" | sed -nE 's/.*gradle-(.+)-(bin|all)\.zip/\1/p')
+  if echo "$GRADLE_VERSION" | grep -qE '^[0-9]+\.[0-9]+$'; then
+    GRADLE_VERSION="${GRADLE_VERSION}.0"
+  fi
   JAR_URL="https://github.com/gradle/gradle/raw/refs/tags/v${GRADLE_VERSION}/gradle/wrapper/gradle-wrapper.jar"
   curl -s -L -o "$WRAPPER_JAR" "$JAR_URL"
   echo "gradle-wrapper.jar downloaded successfully."
